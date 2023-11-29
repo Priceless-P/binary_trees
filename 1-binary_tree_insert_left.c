f136e15c82fa6a8646718b2250c69fccb9a21d75
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-11-29 20:28:25 +0300

    complete task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts the left node
+ * @parent: the parent node
+ * @value: the value of the node
+ * Return: the va;lue node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left_new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	left_new_node = malloc(sizeof(binary_tree_t));
+	if (left_new_node == NULL)
+		return (NULL);
+	left_new_node->n = value;
+	left_new_node->parent = parent;
+	if (parent->left != NULL)
+	{
+		left_new_node->left = parent->left;
+		left_new_node->left->parent = left_new_node;
+	}
+	parent->left = left_new_node;
+	return (left_new_node);
+}
