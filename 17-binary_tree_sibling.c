db8d609d106566d777bf3b83387f4d20addbf9a2
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-11-30 12:45:48 +0100

    task 16,17,18 done

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - Finds the sibling of a node
+* @node: Pointer to the root node of the tree
+* Return: Sibling node or NULL if:
+*   - node is NULL
+*   - or the parent is NULL
+*   - node has no sibling
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	binary_tree_t *parent = node->parent;
+
+	if (parent->left == node)
+		return (parent->right);
+	else if (parent->right == node)
+		return (parent->left);
+
+	return (NULL);
+}
---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+/**
+* binary_tree_uncle - Finds the uncle of a node
+* @node: Pointer to the root node of the tree
+* Return: Sibling node or NULL if:
+*   - node is NULL
+*   - or the parent is NULL
+*   - node has no sibling
+*/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	binary_tree_t *g_parent = node->parent->parent;
+	binary_tree_t *parent = node->parent;
+
+	if (g_parent->left == parent)
+	{
+		if (g_parent->right != NULL)
+			return (g_parent->right);
+	}
+	else if (g_parent->right == parent)
+	{
+		if (g_parent->left != NULL)
+			return (g_parent->left);
+	}
+
+	return (NULL);
+}
