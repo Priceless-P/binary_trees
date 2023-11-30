137ae40f7467aa1eb6c237c5747fc0aabba39673
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-11-30 12:40:23 +0100

    task 14 and 15 done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,8 +2,9 @@
 
 /**
 * binary_tree_height - Measures the height of a binary tree
-* @tree: Pointer to the root node of the tree
-* Return: Height of the tree or 0 if tree is NULL
+* @tree: Pointer to the root node of the tree to measure the height
+*
+* Return: Height of the tree, 0 if tree is NULL
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
